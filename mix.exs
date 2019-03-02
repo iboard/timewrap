04bba4bc4d97cbc552547fd0c927a3eb58bd6ea3
Andreas Altendorfer <andreas@altendorfer.at>
2019-03-02 19:08:11 +0100

    Bump version for travis

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Timewrap.MixProject do
   def project do
     [
       app: :timewrap,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
